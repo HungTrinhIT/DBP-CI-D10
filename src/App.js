77e0fbfdefd0a0baeae1bc7189a314ebc2acad2b
HungTrinh99 <minhhung.it99@gmail.com>
2021-08-28 19:06:12 +0700

    update new thign

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
-
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Home from "./Containers/Home";
+import About from "./Containers/About";
+import UserDetail from "./Containers/UserDeail";
 class App extends Component {
+  state = {
+    users: [],
+  };
+  async componentDidMount() {
+    const data = await axios.get(
+      "https://api.github.com/search/users?q=hungtrinh"
+    );
+    this.setState({
+      users: data.data,
+    });
+  }
   render() {
     return (
-      <div>
-        <p>AppJS here</p>
-      </div>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/user">User</Link>
+
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/user" component={UserDetail} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Containers/UserDeail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class UserDetail extends Component {
+  render() {
+    return <div>UserDetail</div>;
+  }
+}
+
+export default UserDetail;
